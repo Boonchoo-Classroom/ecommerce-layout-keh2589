2a2e4e4ff9c7121b76da5d0267bb070e2d8cf003
keh <sasipim.sa@ku.th>
2025-02-24 02:38:00 +0700

    6530202498 ศศิพิมพ์ สายประดิษฐ์ HOME3

--- app/src/main/java/scisrc/mobiledev/ecommercelayout/MainActivity.kt
+++ app/src/main/java/scisrc/mobiledev/ecommercelayout/MainActivity.kt
@@ -5,14 +5,18 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
 import scisrc.mobiledev.ecommercelayout.databinding.ActivityMainBinding
-import scisrc.mobiledev.ecommercelayout.ui.HomeFragment
+import scisrc.mobiledev.ecommercelayout.ui.Home.HomeFragment
+import scisrc.mobiledev.ecommercelayout.ui.Product_List.ProductFragment
+import scisrc.mobiledev.ecommercelayout.ui.Shopping_cart.ShoppingFragment
+import scisrc.mobiledev.ecommercelayout.ui.Favorites.FavoritesFragment
+import scisrc.mobiledev.ecommercelayout.ui.Profile.ProfileFragment
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        // Handle NavigationView item clicks
+        // Navigation Drawer Listener
         binding.navView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.nav_home -> {
@@ -44,7 +48,26 @@ class MainActivity : AppCompatActivity() {
                         .replace(R.id.fragment_container, HomeFragment())
                         .commit()
                 }
-
+                R.id.nav_product -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ProductFragment())
+                        .commit()
+                }
+                R.id.nav_Shopping_Cart -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ShoppingFragment())
+                        .commit()
+                }
+                R.id.nav_Favorites -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, FavoritesFragment())
+                        .commit()
+                }
+                R.id.nav_Profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ProfileFragment())
+                        .commit()
+                }
             }
             drawerLayout.closeDrawer(GravityCompat.START)
             true
@@ -57,9 +80,5 @@ class MainActivity : AppCompatActivity() {
                 .commit()
             binding.navView.setCheckedItem(R.id.nav_home)
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/scisrc/mobiledev/ecommercelayout/ShoppingFragment.kt
+++ app/src/main/java/scisrc/mobiledev/ecommercelayout/ShoppingFragment.kt
@@ -1,30 +1,27 @@
-package scisrc.mobiledev.ecommercelayout.ui
+package scisrc.mobiledev.ecommercelayout.ui.Shopping_cart
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import scisrc.mobiledev.ecommercelayout.databinding.FragmentHomeBinding
+import scisrc.mobiledev.ecommercelayout.databinding.FragmentShoppingBinding
 
-class HomeFragment : Fragment() {
+class ShoppingFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentShoppingBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // ONLY inflate layout here
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentShoppingBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    // function to do the action in this fragment
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
-
 }
